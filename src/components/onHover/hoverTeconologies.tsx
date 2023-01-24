f7fcfd5bd378463ab2395d249a95d59e78ef8178
Thiago de Oliveira <thiago.silva@ice.ufjf.br>
2023-01-24 09:34:26 -0300

    Adding the boxBadges div and changing the flex direction

--- src/components/onHover/hoverTeconologies.tsx
+++ src/components/onHover/hoverTeconologies.tsx
@@ -2,6 +2,7 @@ import React, { useState, FC } from 'react';
 import { isTemplateSpan } from 'typescript';
 import styles from '../../pages/Home/Home.module.css'
 import AOS from 'aos';
+import { findByLabelText } from '@testing-library/react';
 
 interface Props {
     items: {
@@ -21,28 +22,30 @@ const ShowTextOnHover = ({ items, styles }: Props) => {
 
     return (
         <>
-            <div className={styles.skillBox} data-aos='fade-up' data-aos-duration="2000">
-                {items.map(item => (
-                    <div
-                        className={styles.devBadges}
-                        key={item.id}
-                        onMouseEnter={() => {
-                            setHoveringItemId(item.id);
-                            setHoveringItemText(item.text);
-                        }}
-                        onMouseLeave={() => {
-                            setHoveringItemId(null);
-                            setHoveringItemText(null);
-                        }}
-                    >
-                        <img src={item.icon}
-                            alt={item.id}
-                        />
-                    </div>
-                ))}
-            </div>
-            <div className={styles.articleArea2} data-aos='fade-up' data-aos-duration="2000">
-                {hoveringItemText ? <p>{hoveringItemText}</p> : <p>Passe o mouse para saber mais sobre as tecnologias</p>}
+            <div className={styles.boxBadges}>
+                <div className={styles.skillBox} data-aos='fade-up' data-aos-duration="2000">
+                    {items.map(item => (
+                        <div
+                            className={styles.devBadges}
+                            key={item.id}
+                            onMouseEnter={() => {
+                                setHoveringItemId(item.id);
+                                setHoveringItemText(item.text);
+                            }}
+                            onMouseLeave={() => {
+                                setHoveringItemId(null);
+                                setHoveringItemText(null);
+                            }}
+                        >
+                            <img src={item.icon}
+                                alt={item.id}
+                            />
+                        </div>
+                    ))}
+                </div>
+                <div className={styles.articleArea2} data-aos='fade-up' data-aos-duration="2000">
+                    {hoveringItemText ? <p>{hoveringItemText}</p> : <p>Passe o mouse para saber mais sobre as tecnologias</p>}
+                </div>
             </div>
         </>
     );
