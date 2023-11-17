9aaa6c04bee510c2e5671ab89c475e74e4b92671
Thiago de Oliveira <thiago.silva@ice.ufjf.br>
2023-11-17 10:31:49 -0300

    adding a new project (ygo-vue database)

--- src/components/BtnMain/index.tsx
+++ src/components/BtnMain/index.tsx
@@ -3,7 +3,7 @@ import styles from '../BtnMain/BtnMain.module.css';
 export const BtnMain = () => {
     return (
         <div className={styles.btnArea}>
-            <a href='https://www.linkedin.com/in/thiago-oliveira-5a171426a/' target="_blank">
+            <a href='https://www.linkedin.com/in/thiago-razor/' target="_blank">
                 <div className={styles.btnM}>
                     Linkedin
                 </div>
